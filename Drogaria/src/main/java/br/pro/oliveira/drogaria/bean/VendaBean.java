ad666ab55a6b86980162cbc2daac9ce5e69b62c4
GiovaneOliveiraTI <giovaneoliveira_ti@hotmail.com>
2016-08-30 20:57:17 -0300

    Implementando Venda.

--- Drogaria/src/main/java/br/pro/oliveira/drogaria/bean/VendaBean.java
+++ Drogaria/src/main/java/br/pro/oliveira/drogaria/bean/VendaBean.java
@@ -1,6 +1,7 @@
 package br.pro.oliveira.drogaria.bean;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,15 +15,25 @@ import org.omnifaces.util.Messages;
 import br.pro.oliveira.drogaria.dao.ProdutoDAO;
 import br.pro.oliveira.drogaria.domain.ItemVenda;
 import br.pro.oliveira.drogaria.domain.Produto;
+import br.pro.oliveira.drogaria.domain.Venda;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class VendaBean implements Serializable {
+	private Venda venda;
+
 	private List<Produto> produtos;
 	private List<ItemVenda> itensVenda;
+
+	public Venda getVenda() {
+		return venda;
+	}
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
 	
-	
+
 	public List<ItemVenda> getItensVenda() {
 		return itensVenda;
 	}
@@ -34,37 +45,85 @@ public class VendaBean implements Serializable {
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
-	
+
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
-	
+
 	@PostConstruct
-	public void listar() {
+	public void novo() {
 		try {
+			venda = new Venda();
+			venda.setPrecoTotal(new BigDecimal("0.00"));
+			
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar("descricao");
-			
+
 			itensVenda = new ArrayList<ItemVenda>();
+
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar carregar a tela de vendas");
 			erro.printStackTrace();
 		}
 	}
-	
-	public void adicionar(ActionEvent evento){
-		
-		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
-		ItemVenda itemVenda = new ItemVenda();
-		
-		itemVenda.setPrecoParcial(produto.getPreco());
-		itemVenda.setProduto(produto);
-		itemVenda.setQuantidade(new Short("1"));
-		
-		itensVenda.add(itemVenda);
-		
+
+	public void adicionar(ActionEvent evento) {
+		Produto produto = (Produto) evento.getComponent().getAttributes()
+				.get("produtoSelecionado");
+
+		int achou = -1;
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			if (itensVenda.get(posicao).getProduto().equals(produto)) {
+				achou = posicao;
+
+			}
+
+		}
+
+		if (achou < 0) {
+			ItemVenda itemVenda = new ItemVenda();
+			itemVenda.setPrecoParcial(produto.getPreco());
+			itemVenda.setProduto(produto);
+			itemVenda.setQuantidade(new Short("1"));
+
+			itensVenda.add(itemVenda);
+
+		} else {
+			ItemVenda itemVenda = itensVenda.get(achou);
+			itemVenda.setQuantidade(new Short(itemVenda.getQuantidade() + 1
+					+ ""));
+			itemVenda.setPrecoParcial(produto.getPreco().multiply(
+					new BigDecimal(itemVenda.getQuantidade())));
+		}
+		calcular();
 	}
-	public void retirar(ActionEvent evento){
-		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
+
+	public void retirar(ActionEvent evento) {
+		ItemVenda itemVenda = (ItemVenda) evento.getComponent().getAttributes()
+				.get("itemSelecionado");
+
+		int achou = -1;
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			if (itensVenda.get(posicao).getProduto()
+					.equals(itemVenda.getProduto())) {
+
+			}
+		}
+		if (achou > -1) {
+
+			itensVenda.remove(achou);
+
+		}
+		calcular();
+		
+      
 	}
+	 public void calcular(){
+		 venda.setPrecoTotal(new BigDecimal("0.00"));
+		 for(int posicao = 0; posicao < itensVenda.size(); posicao ++){
+			 ItemVenda itemVenda = 	itensVenda.get(posicao);
+			 venda.setPrecoTotal(venda.getPrecoTotal().add(itemVenda.getPrecoParcial()));
+			 
+		 }
+	 }
 }
